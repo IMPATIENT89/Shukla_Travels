501f489994f6f8bf737c5fc34bbd1bd0c39fa0b9
IMPATIENT89 <ankitshukla8948328509@gmail.com>
2023-03-29 01:07:15 +0530

    change in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,9 +58,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid￼
-  ￼
-￼ inadvertent exposure of personally identifiable information (PII).
+  # information to avoid￼inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
