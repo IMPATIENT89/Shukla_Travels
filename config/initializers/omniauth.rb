49c37984c0082f0cc8d09ac1bbd629c09960f370
Ankit Shukla <96184295+IMPATIENT89@users.noreply.github.com>
2023-12-29 22:06:03 +0530

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'open-uri'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '376842932745-snntihsnthhvp0f04utcq669t2rvu4r0.apps.googleusercontent.com', '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'
+            provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET_KEY']
 end
