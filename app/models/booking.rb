cbebead449180922ac68323ba3800a98e10a6c92
Ankit Shukla <ankitshukla8948328509@gmail.com>
2024-07-12 17:28:09 +0530

    ui changes for whoe project

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,11 +5,19 @@ class Booking < ApplicationRecord
     validates :booking_from, presence: true
     validates :booking_to, presence: true
     validates :vehicle_id, presence: true
+    validates :booking_date, presence: true
     validate :booking_date_not_in_past
+    validate :origin_address_and_destination_address_not_same
 
     def booking_date_not_in_past
-      if booking_date.present? && booking_date < Time.now
+      if booking_date < Time.now
         errors.add(:booking_date, "can't be in the past")
+      end
+    end
+
+    def origin_address_and_destination_address_not_same
+      if booking_from == booking_to
+        errors.add(:booking_from, "and destination address can't be same")
       end
     end
 end 
